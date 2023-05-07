a9b416382de775c07d337caa345b95336237ac7c
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-05-07 10:42:45 -0500

    imprime saludo

---
+++ imprimeNombre.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class imprimeNombre{
+	public static void main (String []args){
+	
+		Scanner sc = new Scanner (System.in);
+		String nombre=" ";
+		System.out.print("ingresa tu nombre: ");
+		nombre=sc.next();
+
+		System.out.println("HOLA "+nombre);
+	}
+}
